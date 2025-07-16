04a336a8573c6cd96b6d3dfb107505fb087b040a
Antônio Rocha da Silva <antoniorochadasilvaa@gmail.com>
2025-07-16 16:24:21 -0300

    Update no hello_world

--- hello_world.dart
+++ hello_world.dart
@@ -1,13 +1,12 @@
 import 'dart:io';
 
 void main() {
+
+  print('Insira seu nome: ');
+
   String? input = stdin.readLineSync();
 
   //Inicio uma variável que possa ser String
-  String? nome;
-
-  print('Insira seu nome: ');
-  stdout.write('Olá, $input');
   
   //caso queira tirar a variável de nula
   //nome = 'Pãozinho';
@@ -15,11 +14,8 @@ void main() {
   //funciona quase como um if/else, basicamente cliente recebe o valor que tiver em nome,
   //porém, se nome não tiver dados (for nulo), ele por meio do ?? receberá um valor "template"
   //sendo esse valor o "Usuário", assim cliente recebe Usuário por padrão.
-  String cliente = nome ?? "Usuário";
+  String cliente = input ?? "Usuário";
 
-
-  
-
-  
+  print('Seja bem vindo cliente ${cliente}!');
 
 }
