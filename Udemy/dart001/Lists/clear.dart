df6be130d4b4d2b6b06517fb6476feeb46661327
paozinho6254 <antoniorochadasilvaa@gmail.com>
2025-07-16 21:04:47 -0300

    organizacao das pastas

---
+++ Udemy/dart001/Lists/clear.dart
@@ -0,0 +1,18 @@
+  void main() {
+  
+  List<int> precos = [
+    20,
+    53,
+    645,
+    89,
+    13,
+    65,
+    67
+  ];
+  
+  precos.clear();
+  
+  //A principio não é para aparecer nada
+  print(precos);
+  
+}
---
+++ Udemy/dart001/Lists/shuffle.dart
@@ -0,0 +1,10 @@
+void main() {
+  
+  List<int> precos = [ 20, 53, 645, 89, 13, 65, 67];
+  
+  precos.shuffle();
+
+  //O resultado esperado é que a lista misture a posição destes valores
+  print(precos);
+  
+}
