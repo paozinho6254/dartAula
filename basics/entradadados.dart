ef2b6c34eb491419178caf89f8e0359a89eacc40
Antônio Rocha da Silva <antoniorochadasilvaa@gmail.com>
2025-07-21 15:03:12 -0300

    adicionei novos arquivos dart

---
+++ basics/entradadados.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(){
+  //Entrada de dados
+  //informando o nome
+  stdout.write("Diga seu nome! ");
+  String? nome = stdin.readLineSync();
+
+  //informando a idade
+
+}
---
+++ basics/tiposdados.dart
@@ -0,0 +1,12 @@
+void main(){
+  int idade = 10;
+  double salario = 1200.55;
+  String nomePessoa = 'Fernando';
+  bool estaChovendo = false;
+
+  print(
+    "Nome: $nomePessoa, Idade: $idade, Salario: $salario, Chovendo? $estaChovendo"
+  );
+
+
+}
