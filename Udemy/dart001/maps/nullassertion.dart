bc93bfbd82e29d38cf587160b514d76220cd3772
Antônio Rocha da Silva <134741768+paozinho6254@users.noreply.github.com>
2025-08-04 17:12:16 -0300

    Adicionado novas coisas

--- Udemy/dart001/maps/nullassertion.dart
+++ Udemy/dart001/maps/nullassertion.dart
@@ -8,14 +8,30 @@ void main() {
     51: 'Rio Grande do Sul',
   };
 
-  //só testando 
+  ddds.addAll({90: 'Distrito do Roubo', 20 : 'Senerasal do Norte'});
+
   ddds.forEach((key, value) {
     // Cria variável valor, que ao receber key, ele a testa se é igual a 21
     // caso sim, o ternário define que o valor será convertido para maiúsculas
     // : value é caso não seja a chave 21
     String valor = (key == 21) ? value.toUpperCase() : value;
-    print('Chave $key Valor $valor');
+
+    //Aqui foi para informar o teste do comando .addAll
+    if(key == 90){
+      print("\nChaves novas adicionadas:");
+    }
+    print('Chave $key Valor $value');
   });
+  
 
+  // remove onde key (ddd) for maior que 31
+  ddds.removeWhere((key, value) => key > 31);
 
+  print("Removido ddds acima de 31: \n$ddds");
+
+  //este é o Null Assertion 
+  //Aqui indico que cidade nunca será nula
+  String cidade = ddds[11]!;
+
+  print(cidade);
 }
