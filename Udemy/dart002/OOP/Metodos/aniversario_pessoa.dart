6370a4281b7f94f0973140d76a722d6295bf8aea
Antônio Rocha da Silva <134741768+paozinho6254@users.noreply.github.com>
2025-08-06 10:00:03 -0300

    Update aniversario_pessoa.dart

--- Udemy/dart002/OOP/Metodos/aniversario_pessoa.dart
+++ Udemy/dart002/OOP/Metodos/aniversario_pessoa.dart
@@ -1,8 +1,12 @@
 void main(){
 
   Pessoa p1 = Pessoa();
+  Esposa e1 = Esposa();
   p1.nome = "Pãozinho";
   p1.idade = 18;
+  
+  e1.nomeEsposa = "Nathally";
+  e1.idade = 18;
 
   print("Nome: ${p1.nome}");
   print("Idade: ${p1.idade}");
@@ -10,14 +14,19 @@ void main(){
   p1.aniversario();
 
   print("${p1.nome} agora você tem ${p1.idade} anos.");
-
+  
+  p1.casar(e1.nomeEsposa);
+  
+  print('${p1.nome} você terá seu nome trocado para: ${p1.trocarNome()}');
+  
 }
 
 class Pessoa{
 
   String? nome;
   int? idade;
-
+  bool? casado = false;
+  
   void aniversario() {
     if (idade != null) {
       //Não, idade++ não da certo, pois idade pode ser nulo
@@ -28,5 +37,24 @@ class Pessoa{
       print("Idade não definida para $nome.");
     }
   }
+  
+  
+  void casar(nomeEsposa){
+    casado = true;
+    print('Parabéns pelo casamento com $nomeEsposa');
+  }
+  
+  String trocarNome(){
+    return nome = 'Antônio';
+  }
+  
+  void exibirDados(){
+     //coloca pra exibir os dados do cara
+  }
+  
+}
 
+class Esposa{
+  String? nomeEsposa;
+  int? idade; 
 }
