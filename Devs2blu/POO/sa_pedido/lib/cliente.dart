c2f9d3ce8e32f8f31a7a6515ec9d6ccf75f2f4d4
Antônio Rocha da Silva <134741768+paozinho6254@users.noreply.github.com>
2025-08-21 09:59:16 -0300

    sa continua

---
+++ Devs2blu/POO/sa_pedido/lib/cliente.dart
@@ -0,0 +1,31 @@
+class Cliente {
+  int idCliente;
+  String nomeCliente;
+  String cpfCliente;
+
+  Cliente({
+    required this.idCliente,
+    required this.nomeCliente,
+    required this.cpfCliente,
+  });
+
+  get getId {
+    return idCliente;
+  }
+
+  set setNome(String nome) {
+    nomeCliente = nome;
+  }
+
+  get getNome {
+    return nomeCliente;
+  }
+
+  set setCPF(String cpf) {
+    cpfCliente = cpf;
+  }
+
+  get getCPF {
+    return cpfCliente;
+  }
+}
---
+++ Devs2blu/POO/sa_pedido/lib/tipo_produto.dart
@@ -0,0 +1,19 @@
+enum TipoProduto {
+  eletronico,
+  alimento, 
+  quimico,
+  biologico,
+  metais,
+  material
+}
+
+enum EspProduto{
+  fragil,
+  toxico,
+  inflamavel,
+  explosivo,
+  corrosivo,
+  radioativo,
+  biologico,
+  sensivelTemperatura
+}
