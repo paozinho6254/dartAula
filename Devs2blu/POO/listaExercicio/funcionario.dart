7cdfb78ebd07610717df6c712e215c038cffffaa
Antônio Pãozinho Rocha da Silva <134741768+paozinho6254@users.noreply.github.com>
2025-09-08 09:56:35 -0300

    terminar ainda

--- Devs2blu/POO/listaExercicio/funcionario.dart
+++ Devs2blu/POO/listaExercicio/funcionario.dart
@@ -1,27 +1,23 @@
-//Crie uma classe abstrata `Funcionario` com o método `calcularSalario()`. 
-//Crie duas subclasses `Desenvolvedor` e `Gerente` 
+//Crie uma classe abstrata `Funcionario` com o método `calcularSalario()`.
+//Crie duas subclasses `Desenvolvedor` e `Gerente`
 //com implementações diferentes do cálculo do salário.
 
 abstract class Funcionario {
-  int? _id;
-  String? _nome;
-  
+  int _id;
+  String _nome;
+  double _salarioBase;
 
-  Funcionario({int? id, String? nome}) {
-    this._id = id ?? 0;
-    this._nome = nome ?? "Não informado";
-  }
+  Funcionario(this._id, this._nome, this._salarioBase);
 
   double calcularSalario();
 }
 
 class Gerente extends Funcionario {
-  
-  double? _salarioBase;
   double? _bonus;
 
-  Gerente({int? id, String? nome, double? salarioBase, double? bonus}) : _salarioBase = salarioBase, _bonus = bonus, super(id: id, nome: nome);
-
+  Gerente({int? id, String? nome, double? salarioBase, double? bonus})
+    : _bonus = bonus,
+      super(id: id, nome: nome, salarioBase: salarioBase);
 
   @override
   double calcularSalario() {
@@ -30,47 +26,47 @@ class Gerente extends Funcionario {
 }
 
 class Desenvolvedor extends Funcionario {
-
-  double? _salarioBase;
   double? _beneficios;
 
   Desenvolvedor({
-    int? id, 
+    int? id,
     String? nome,
     double? salarioBase,
-    double? beneficios
-  }) :
-  _salarioBase = salarioBase,
-  _beneficios = beneficios,
-  super(
-    id: id,
-    nome: nome
-  );
+    double? beneficios,
+  }) : _beneficios = beneficios,
+       super(id: id, nome: nome, salarioBase: salarioBase);
 
   @override
-  double calcularSalario(){
+  double calcularSalario() {
     return _salarioBase! - _beneficios!;
   }
-
 }
 
-void main(){
-
+void main() {
   Desenvolvedor dev = Desenvolvedor(
     id: 1000,
     nome: 'Pãozinho',
     salarioBase: 2000,
-    beneficios: 650
+    beneficios: 650,
   );
 
   Gerente gerente = Gerente(
     id: 200,
     nome: 'Jeca',
     salarioBase: 5000,
-    bonus: 250
+    bonus: 250,
   );
 
-  print("Salário do desenvolvedor de de id ${dev._id}: ${dev.calcularSalario()}");
-  print("Salário do gerente de id ${gerente._id}: ${gerente.calcularSalario()}");
+  List<Funcionario> funcionarios = [gerente, dev];
 
+  for (Funcionario funca in funcionarios) {
+    print(funca);
+  }
+
+  print(
+    "Salário do desenvolvedor de de id ${dev._id}: ${dev.calcularSalario()}",
+  );
+  print(
+    "Salário do gerente de id ${gerente._id}: ${gerente.calcularSalario()}",
+  );
 }
