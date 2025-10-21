1c2fde75540432efb62558ab1724474d1f5ea646
ANTÔNIO ROCHA DA SILVA <antonio_silva26@estudante.sesisenai.org.br>
2025-10-21 17:21:51 -0300

    mais função para o botão

--- Udemy/flutter_projetos/telabasica/lib/main.dart
+++ Udemy/flutter_projetos/telabasica/lib/main.dart
@@ -22,68 +22,80 @@ class HomePage extends StatelessWidget {
 
   void _saiu() {
     print('Decrementou');
-    // if(pessoas > 0){
+    // if (pessoas > 0) {
     //   pessoas--;
     // }
   }
 
   @override
   Widget build(BuildContext context) {
-    int pessoas = 200;
+    int pessoas = 0;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Recebendo!',
-            style: TextStyle(fontSize: 48, color: Colors.white),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            //image: NetworkImage('https://akamai.sscdn.co/uploadfile/letras/albuns/f/a/e/2/791351570015623.jpg'),
+            image: AssetImage('assets/images/Avion.jpg'),
+            fit: BoxFit.cover
           ),
-          Text('$pessoas', style: TextStyle(color: Colors.white, fontSize: 32)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: _entrou,
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(125, 125),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(30),
-                      side: BorderSide(
-                          color: Colors.green, width: 5
-                      )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Recebendo!',
+              style: TextStyle(fontSize: 48, color: Colors.white),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(48),
+              child: Text(
+                '0',
+                style: TextStyle(color: Colors.white, fontSize: 100),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: _entrou,
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(125, 125),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(30),
+                      side: BorderSide(color: Colors.green, width: 5),
+                    ),
+                  ),
+                  child: const Text(
+                    "Entrou",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
                   ),
                 ),
-                child: Text(
-                  "Entrou",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
+                const SizedBox(width: 30),
+                TextButton(
+                  onPressed: _saiu,
+                  // basicamente o padding é o espaço que o texto ocupa em relação as bordas do botão
+                  // a constante EdgeInsets.all(32) diz que vai acrescentar 32 pixels de espaço para todos os lados.
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(125, 125),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(30),
+                      side: BorderSide(color: Colors.red, width: 5),
+                    ),
+                  ),
+                  child: const Text(
+                    "Saiu",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: _saiu,
-                // basicamente o padding é o espaço que o texto ocupa em relação as bordas do botão
-                // a constante EdgeInsets.all(32) diz que vai acrescentar 32 pixels de espaço para todos os lados.
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(125, 125),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(30),
-                    side: BorderSide(
-                      color: Colors.red, width: 5,
-                    )
-                  )
-                ),
-                child: Text(
-                  "Saiu",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
