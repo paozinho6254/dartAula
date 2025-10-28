7cd866f8f445b55e89a2765f388ec0b3ccae1245
ANTÔNIO ROCHA DA SILVA <antonio_silva26@estudante.sesisenai.org.br>
2025-10-28 14:11:43 -0300

    adicionando mais funções

--- Udemy/flutter002/lib/page/todo_list_page.dart
+++ Udemy/flutter002/lib/page/todo_list_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+  
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
 
+  // final TextEditingController emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red, child: Column());
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          ],
+        ),
+      ),
+    );
   }
 }
+
+/*
+final _cpfFormatter = MaslTextInputFormatter(
+  mask: '###.###.###-##'
+  filter:
+)
+ */
